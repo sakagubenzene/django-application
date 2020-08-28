fdc6a0b551d9226cf2632e4754fba9838541617e
T.S <iby03101600@gmail.com>
2020-08-28 20:22:26 +0900

    #4dayo

--- tweet/views.py
+++ tweet/views.py
@@ -1,11 +1,18 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
 
 
 
 class PostListView(ListView):
     model = Post
-    context_object_name = 'post'
+    context_object_name = 'posts'
     template_name='tweet/index.html'
     ordering=['-date_posted']
     paginate_by=5
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['content']
+    template_name = "tweet/post_create.html"
+    success_url = '/'
